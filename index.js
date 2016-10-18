fa46c85d9558638820853b1a7ccb38b6777af615
Barbara Carvalho <bacs96@gmail.com>
2016-10-18 14:26:54 -0200

    conexao com o bd

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ app.use(function(req, res, next) {
 
 
 //CONEXAO BANCO DE DADOS
-// var mongoose = require ('mongoose');
-//
-// var db = mongoose.connection;
-//
-// db.on('error', console.error);
-// db.once('open', function(){
-//   console.log("Conexao com bd feita com sucesso");
-// });
-//
-// mongoose.connect('mongodb://localhost:27017/test');
+ var mongoose = require ('mongoose');
+
+ var db = mongoose.connection;
+
+ db.on('error', console.error);
+ db.once('open', function(){
+   console.log("Conexao com bd feita com sucesso");
+ });
+
+ mongoose.connect('mongodb://localhost:27017/test');
 
 /////
 
